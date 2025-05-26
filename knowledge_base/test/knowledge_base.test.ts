c2ed5a4810989202c84c4053f5912f3a49054f0f
TaiyouWatanabe03 <watanabe_t@kokopelli-inc.com>
2025-05-26 15:28:12 +0900

    CDkの追加

---
+++ knowledge_base/test/knowledge_base.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KnowledgeBase from '../lib/knowledge_base-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/knowledge_base-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KnowledgeBase.KnowledgeBaseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

d5c2422b0ac523362afc513d16be9ee9c7e31478
TaiyouWatanabe03 <watanabe_t@kokopelli-inc.com>
2025-06-02 10:40:47 +0900

    変更

--- knowledge_base/README.md
+++ knowledge_base/README.md
@@ -12,3 +12,11 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+npx cdk diff --profile aws_poc 
+aws sso login --profile aws_poc
+cat ~/.aws/config
+npx cdk deploy --profile aws_poc
+git status
+git add
+gitb
--- knowledge_base/bin/knowledge_base.ts
+++ knowledge_base/bin/knowledge_base.ts
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { KnowledgeBaseStack } from '../lib/knowledge_base-stack';
+import { BedrockStack } from '../lib/stacks/bedrock-stack';
+// import { OpenSearchStack } from '../lib/stacks/opensearch-stack';
+// import { BedrockStack } from '../lib/stacks/bedrock-stack';
+
 
 const app = new cdk.App();
-new KnowledgeBaseStack(app, 'KnowledgeBaseStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// はじめに、OpenSeaerch Stackをdeployします。
+// const opensearchStack = new OpenSearchStack(app, 'OpenSearchStack', {
+//   // env:{
+//   //   account:process.env.CDK_DEFAULT_ACCOUNT,
+//   //   region:process.env.CDK_DEFAULT_REGION || 'ap-northeast-1'
+//   // }
+// });
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+// 次に、Bedrock Stackをdeployします。
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const bedrockStack = new BedrockStack(app,'BedrockStack', {
+  collectionArn: 'arn:aws:bedrock:us-east-1:111122223333:collection/cdk-collection',
+  bucketArn: 'arn:aws:s3:::cdk-bucket',
+  bedrockRoleArn: 'arn:aws:iam::111122223333:role/iamrole'
 });
